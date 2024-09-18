8cf72fa2caf84f2865ff5d0a66482ba55ae4e1b4
Tim Rood <timdadum@gmail.com>
2024-09-18 12:20:38 +0200

    Change test.py for DAIC inference.

--- miscelleneous/test.py
+++ miscelleneous/test.py
@@ -1,28 +1,21 @@
 import transformers
-import torch
 
-model_id = "meta-llama/Meta-Llama-3.1-8B"
+model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 
-# Initialize the model with necessary configurations
 pipeline = transformers.pipeline(
-    "text-generation", 
-    model=model_id, 
-    model_kwargs={"torch_dtype": torch.bfloat16}, 
-    device_map="auto"
+    "text-generation",
+    model=model_id,
+    device_map="auto",  # Automatically map layers to available GPUs
 )
 
-# Define the system prompt
-system_prompt = "<|SYSTEM|> You are a helpful assistant."
-
-# Define user prompts
-user_prompts = [
-    "<|USER|> What is the weather like today?",
-    "<|USER|> Tell me a joke."
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
 ]
 
-# Combine system and user prompts
-responses = [pipeline(system_prompt + prompt, max_length=1000, truncation=True) for prompt in user_prompts]
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+)
 
-# Print responses
-for response in responses:
-    print(response)
+print(outputs[0]["generated_text"])
